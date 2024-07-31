e812aed7f6ca135a5cd79c5cc3aad4b23526bf66
MbaliPuso <mbalipuso@gmail.com>
2024-07-31 10:11:13 +0200

    listing component

---
+++ src/components/CarListings.jsx
@@ -0,0 +1,287 @@
+import React from "react";
+
+const CarListings = () => {
+    return (
+      <>
+        <div className="site-section bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7">
+                <h2 className="section-heading">
+                  <strong>Car Listings</strong>
+                </h2>
+                <p className="mb-5">
+                  Welcome To Penta Now
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_6.jpg"
+                      alt="Mitsubishi Pajero"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Mitsubishi Pajero</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_5.jpg"
+                      alt="Nissan Moco"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Nissan Moco</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_4.jpg"
+                      alt="Honda Fitta"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Honda Fitta</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_3.jpg"
+                      alt="Skoda Laura"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Skoda Laura</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_2.jpg"
+                      alt="Mazda LaPuta"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Mazda LaPuta</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4 mb-4">
+                <div className="listing d-block  align-items-stretch">
+                  <div className="listing-img h-100 mr-4">
+                    <img
+                      src="images/car_1.jpg"
+                      alt="Buick LaCrosse"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="listing-contents h-100">
+                    <h3>Buick LaCrosse</h3>
+                    <div className="rent-price">
+                      <strong>$389.00</strong>
+                      <span className="mx-1">/</span>day
+                    </div>
+                    <div className="d-block d-md-flex mb-3 border-bottom pb-3">
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Luggage:</span>
+                        <span className="number">8</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Doors:</span>
+                        <span className="number">4</span>
+                      </div>
+                      <div className="listing-feature pr-4">
+                        <span className="caption">Passenger:</span>
+                        <span className="number">4</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Quos eos at eum, voluptatem quibusdam.
+                      </p>
+                      <p>
+                        <a href="/" className="btn btn-primary btn-sm">
+                          Rent Now
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a href="/" className="btn btn-primary btn-sm d-block m-auto w-25">
+                View All Cars
+            </a>
+          </div>
+        </div>
+      </>
+    );
+};
+
+export default CarListings;
