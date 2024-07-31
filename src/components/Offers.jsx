e823e3e0470b25ae5d540c7ea14f94a70c9f247f
MbaliPuso <mbalipuso@gmail.com>
2024-07-31 10:11:51 +0200

    offers section

---
+++ src/components/Offers.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="site-section">
+        <div className="container">
+          <h2 className="section-heading">
+            <strong>What we offer?</strong>
+          </h2>
+          <p className="mb-5">At Penta Now, we provide top-quality pre-owned vehicles that are performance-verified. Our services include flexible financing, vehicle buy-offs, trade-ins, and expert servicing. Discover your perfect car with us today!</p>
+          <div className="row mb-5">
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
+                <span>1</span>
+                <div className="step-inner">
+                  <span className="number text-primary">01.</span>
+                  <h3>We Sell Quality Used Cars</h3>
+                  <p>
+                  We deliver exclusive deals and quality cars at great prices. <br />
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
+                <span>2</span>
+                <div className="step-inner">
+                  <span className="number text-primary">02.</span>
+                  <h3>We Buy Used Vehicles</h3>
+                  <p>
+                  We offer instant cash for used vehicles - With a quick process that pays well, Anywhere | Anytime
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
+                <span>3</span>
+                <div className="step-inner">
+                  <span className="number text-primary">03.</span>
+                  <h3>Vehicle Financing</h3>
+                  <p>
+                  Explore hassle-free Vehicle Financing - Our experts are ready to tailor the perfect financing plan to drive your dreams forward.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
