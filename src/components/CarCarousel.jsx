4d31b9ce57d25de9333cf226608247e7bf8f9be5
MbaliPuso <mbalipuso@gmail.com>
2024-08-07 08:58:47 +0200

    carousel update

--- src/components/CarCarousel.jsx
+++ src/components/CarCarousel.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Image, Modal, Button } from "react-bootstrap";
+import "../styles/CarCarousel.css";
 import image1 from "../assets/nissan-qashqai.jpg";
 import image2 from "../assets/Honda_Jazz.jpg";
 import image3 from "../assets/Mercedes-Benz-B-class.jpg";
 import image4 from "../assets/chevrolet-utility.jpg";
 import image5 from "../assets/now-banner.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes  } from "@fortawesome/free-solid-svg-icons";
 
 const images = [image1, image2, image3, image4, image5];
 
 const CarCarousel = () => {
-    const [photoIndex, setPhotoIndex] = useState(0);
-  
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
     <>
-      
       <div className="container">
         <Carousel
           activeIndex={photoIndex}
           onSelect={(selectedIndex) => setPhotoIndex(selectedIndex)}
           indicators={false}
+          interval={null}
         >
           {images.map((image, index) => (
             <Carousel.Item key={index}>
@@ -26,10 +33,14 @@ const CarCarousel = () => {
                 src={image}
                 alt={`Car ${index}`}
                 className="d-block w-100"
+                onClick={handleShowModal}
+                style={{ cursor: "pointer" }}
               />
             </Carousel.Item>
           ))}
         </Carousel>
+
+        <hr />
 
         <div className="thumbnail-images">
           {images.map((image, index) => (
@@ -39,10 +50,24 @@ const CarCarousel = () => {
               alt={`Thumbnail ${index}`}
               className="img-thumbnail"
               onClick={() => setPhotoIndex(index)}
-              style={{ cursor: "pointer", width: "100px", height: "auto" }}
+              style={{ cursor: "pointer", width: "100px", height: "80px" }}
             />
           ))}
         </div>
+        <Modal show={showModal} onHide={handleCloseModal} centered>
+          <Modal.Header className="border-0">
+            <Button
+              variant="light"
+              className="ms-auto"
+              onClick={handleCloseModal}
+            >
+              <FontAwesomeIcon icon={faTimes} />
+            </Button>
+          </Modal.Header>
+          <Modal.Body className="text-center">
+            <Image src={images[photoIndex]} alt={`Car ${photoIndex}`} fluid />
+          </Modal.Body>
+        </Modal>
       </div>
     </>
   );
